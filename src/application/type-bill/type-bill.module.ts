36a02e99daa1288ecf32592c11b2b072669543c3
Rodrigo <rodrigo.santos@grupoicts.com.br>
2024-04-11 16:51:50 -0400

    feat: config files

---
+++ src/application/type-bill/type-bill.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeBillController } from './type-bill.controller';
+import { TypeBillService } from './type-bill.service';
+
+@Module({
+  controllers: [TypeBillController],
+  providers: [TypeBillService]
+})
+export class TypeBillModule {}
