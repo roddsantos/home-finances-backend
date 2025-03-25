55383abf878dd9d9ea26973458402efb61a9ce55
Rodrigo <rodrigo.santos@grupoicts.com.br>
2025-03-24 20:36:07 -0400

    feat: module and services

--- src/application/auth/auth.service.ts
+++ src/application/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { UserService } from '../user/user.service'
 import * as bcrypt from 'bcryptjs'
 import { User } from '../user/user.entity'
 import { JwtService } from '@nestjs/jwt'
+import { ErrorHandler } from '../utils/ErrorHandler'
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,26 @@ export class AuthService {
     private readonly jwtService: JwtService
   ) {}
 
+  /**
+   * Hash a password for a User
+   * @param password the password to hash
+   * @returns hash of the password
+   */
+  async hashPassword(password: string) {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      return hashedPassword
+    } catch (error) {
+      return ErrorHandler.handle(error)
+    }
+  }
+
+  /**
+   * Verify if username or password is correct
+   * @param username user's username
+   * @param password user's password
+   * @returns User if user credential is ok, null otherwise
+   */
   async validateUser(username: string, password: string): Promise<User | null> {
     const user = await this.userService.getOne(username)
     if (!user) return null
@@ -20,6 +41,11 @@ export class AuthService {
     else return null
   }
 
+  /**
+   * Create login token
+   * @param user validated user
+   * @returns token
+   */
   async login(user: User) {
     const payload = { username: user.username, sub: user.id }
     const token = this.jwtService.sign(payload)
