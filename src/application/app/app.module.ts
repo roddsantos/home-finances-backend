52a4b98817ffa5bd381d3290d6087b018adadb37
Rodrigo <rodrigo.santos@grupoicts.com.br>
2026-01-29 22:26:36 -0400

    fix: id middlewares

--- src/application/app/app.module.ts
+++ src/application/app/app.module.ts
@@ -82,14 +82,10 @@ export class AppModule implements OnApplicationBootstrap, NestModule {
     consumer
       .apply(IdMiddleware)
       .forRoutes(
-        { path: 'theme', method: RequestMethod.GET },
         { path: 'theme', method: RequestMethod.DELETE },
         { path: 'bill/quick-settle/:id', method: RequestMethod.PATCH },
-        { path: 'category/:id', method: RequestMethod.GET },
         { path: 'category/:id', method: RequestMethod.DELETE },
-        { path: 'bank/:id', method: RequestMethod.GET },
         { path: 'bank/:id', method: RequestMethod.DELETE },
-        { path: 'company/:id', method: RequestMethod.GET },
         { path: 'company/:id', method: RequestMethod.DELETE }
       )
 
